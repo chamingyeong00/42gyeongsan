a32561c775beac9d18ffc5c47d2747f4cb44a820
Mingyeong Cha <micha@c3r13s1.42gyeongsan.kr>
2025-07-11 16:13:20 +0900

    add file

---
+++ get_next_line/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main()
+{
+/*
+    int fd = open("test.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open");
+        return 1;
+    }
+    */
+    int fd = 0;
+
+
+    char *line;
+    while ((line = get_next_line(fd)))
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    close(fd);
+    return 0;
+}
